43046a0c799cfd63ebfaff9d9f193929769b37ea
Son of Forehead <jpstoner@icloud.com>
2024-10-17 12:04:10 -0600

    Saves Email to Directory

---
+++ View/CreatePassword.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace Password_Manager.View
+{
+    public partial class CreatePassword : Window
+    {
+        public string email = "";
+        public string passwordPath = @"Settings\Passwords.txt";
+        public CreatePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            email = EmailText.Text;
+
+            WriteInfo();
+        }
+
+        private void WriteInfo() 
+        {
+            Directory.CreateDirectory(@"Settings\");
+
+            File.WriteAllText(passwordPath, "{ Email: " + email + " }");
+            string info = File.ReadAllText(passwordPath);
+            Console.WriteLine(info);
+            Console.ReadLine();
+        }
+    }
+}
